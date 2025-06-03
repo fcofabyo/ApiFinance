ada1db724251d2507d085140191583f76a2029f2
Fco fabio <franciscofabioinffo@gmail.com>
2025-06-03 10:15:19 -0300

    Project started

---
+++ src/main/java/com/ApiFinance/Apikodax/ApikodaxApplication.java
@@ -0,0 +1,13 @@
+package com.ApiFinance.Apikodax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApikodaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApikodaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ApiFinance/Apikodax/ApikodaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiFinance.Apikodax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApikodaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
